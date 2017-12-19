fb54e03eb28514701287c394da15670d7570a8ec
memeyun2000 <memeyun2000@126.com>
2017-12-19 22:27:37 +0800

    spring boot data connect to derby database

---
+++ Java/Spring/src/_02_spring_schedule_simple2/src/main/java/com/sec/App.java
@@ -0,0 +1,19 @@
+package com.sec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    //     AnnotationConfigApplicationContext rootContext =
+    //  new AnnotationConfigApplicationContext();
+ 
+    // rootContext.register(RootContextConfiguration.class);
+    // rootContext.refresh();
+        System.out.print("hello world");
+        
+    }
+}
