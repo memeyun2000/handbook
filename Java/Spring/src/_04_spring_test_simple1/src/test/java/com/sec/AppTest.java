d0470e5b48f2d809fa9afe089896da0b3021ee62
sec <memeyun2000@126.com>
2017-12-19 11:44:22 +0800

    spring boot test

--- Java/Spring/src/_04_spring_test_simple1/src/test/java/com/sec/AppTest.java
+++ Java/Spring/src/_04_spring_test_simple1/src/test/java/com/sec/AppTest.java
@@ -4,8 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.sec.service.Employee;
@@ -15,7 +14,7 @@ import com.sec.service.Employee;
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootApplication
+@SpringBootTest
 public class AppTest {
 
     @Autowired
