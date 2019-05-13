c5c52beee106637eba3392d9b7544a4c2070a6ae
memeyun2000 <memeyun2000@126.com>
2019-05-14 07:06:37 +0800

    maven assembly for assembly-distribute.xml

---
+++ maven/src/maven-201905/maven-assembly-base/src/main/java/com/sec/profile/App.java
@@ -0,0 +1,17 @@
+package com.sec.profile;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String newStr = StringUtils.substring("hello world",3);
+        System.out.println(newStr);
+    }
+}
